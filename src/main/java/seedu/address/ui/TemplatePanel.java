1a81c08e469b784b495e3c56a2a288f74899f98d
marvintxd <37933324+marvintxd@users.noreply.github.com>
2018-11-12 02:48:31 +0800

    Make template show filepath in UI heading

--- src/main/java/seedu/address/ui/TemplatePanel.java
+++ src/main/java/seedu/address/ui/TemplatePanel.java
@@ -8,10 +8,14 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.storage.TemplateLoadedEvent;
+import seedu.address.model.template.Template;
 
 /**
  * A ui for the display of the loaded template.
@@ -23,6 +27,8 @@ public class TemplatePanel extends UiPart<Region> {
 
     private final StringProperty displayed = new SimpleStringProperty("");
 
+    @FXML
+    private Label templateLabel;
     @FXML
     private TextArea templatePanelDisplay;
 
@@ -37,7 +43,11 @@ public class TemplatePanel extends UiPart<Region> {
     @Subscribe
     private void handleTemplateLoadedEvent(TemplateLoadedEvent event) {
         logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        Platform.runLater(() -> displayed.setValue(event.getTemplate().toString()));
+        Platform.runLater(() -> {
+            Template t = event.getTemplate();
+            templateLabel.setText(String.format("Current Template: %s", t.getFilepath()));
+            displayed.setValue(t.toString());
+        });
     }
 
 }
